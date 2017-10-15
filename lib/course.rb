d619f9cb447ce8a7f6c95bcdfcac8097f3c5d544
Abe Rudy <aberudy@gmail.com>
2017-10-14 21:38:51 -0400

    scraping with nokogiri

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,18 @@
+class Course
+  @@all = []
 
+attr_accessor :schedule, :description, :title
 
+  def initialize()
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    self.all.clear
+  end
+
+end
