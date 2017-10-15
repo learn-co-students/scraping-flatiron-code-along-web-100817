1e0d248489ad40893cc19377ee968060bab18536
Abe Rudy <aberudy@gmail.com>
2017-10-14 21:44:47 -0400

    add comments for references

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,15 +6,15 @@ require_relative './course.rb'
 
 class Scraper
 
-    def get_page
+    def get_page #wrap the open-uri access inside nokogiri
       doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
     end
 
-    def get_courses
+    def get_courses #runs #get_page to pull page data, use nokogiri.css('.post') for each post
       self.get_page.css('.post')
     end
 
-    def make_courses
+    def make_courses #call self.get_courses to get use just the posts, iterate over to assign attributes you our new Course object.
       self.get_courses.each do |post|
         course = Course.new
         course.title = post.css('.post').css('h2').text
